b9d2ed50d6d435428ea558da6e3b6b013a32a44f
wissem189 <wissem.hammouda@esprit.tn>
2023-02-19 16:23:54 +0100

    maj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
 import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OffresEmploiComponent,
-    ArticlesComponentComponent
+    ArticlesComponentComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule,
